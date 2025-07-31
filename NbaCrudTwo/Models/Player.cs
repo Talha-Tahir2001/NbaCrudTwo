7d1126ddd10e96b6c56d1698af8b749732083cc1
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-08-01 01:18:33 +0500

    feat: add a player model
    - add a player data model

---
+++ NbaCrudTwo/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace NbaCrudTwo.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Team { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Position { get; set; } = string.Empty;
+        public string Height { get; set; } = string.Empty;        
+        public string College { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public DateOnly DraftYear { get; set; }        
+    }
+}
